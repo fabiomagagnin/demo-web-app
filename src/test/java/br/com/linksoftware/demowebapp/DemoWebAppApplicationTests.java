5dd2b45f500216c4a83e068beda00c75b73c5d1b
Fábio Magagnin <fabio.email.123@gmail.com>
2019-04-02 20:47:14 -0300

    first commit

---
+++ src/test/java/br/com/linksoftware/demowebapp/DemoWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.linksoftware.demowebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
