650a07af810515576da6ce0a79291d5993356f3c
Fábio Magagnin <fabio.email.123@gmail.com>
2019-04-02 21:13:33 -0300

    Controller and index

---
+++ src/main/java/br/com/linksoftware/demowebapp/DemoController.java
@@ -0,0 +1,27 @@
+package br.com.linksoftware.demowebapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class DemoController {
+	
+	@GetMapping("person/{id}")
+	public Person getPerson(@PathVariable Long id) {
+		return new Person("Person " + id, "51 9 9101 1255");
+	}
+	
+	@GetMapping("person")
+	public Person getPerson() {
+		return new Person("Fabio", "51 9 9101 1255");
+	}
+	
+	@GetMapping("person2")
+	public Person getPerson2() {
+		return new Person("Fabio 2", "51 9 9101 1255");
+	}	
+
+}
