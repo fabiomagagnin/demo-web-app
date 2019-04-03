da8d05fa1b125887e99bf6b574c58cdda2a41c10
Fábio Magagnin <fabio.email.123@gmail.com>
2019-04-02 22:06:27 -0300

    Hora

--- src/main/java/br/com/linksoftware/demowebapp/DemoController.java
+++ src/main/java/br/com/linksoftware/demowebapp/DemoController.java
@@ -1,5 +1,7 @@
 package br.com.linksoftware.demowebapp;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ public class DemoController {
 	
 	@GetMapping("person/{id}")
 	public Person getPerson(@PathVariable Long id) {
-		return new Person("Person " + id, "51 9 9101 1255");
+		return new Person("Person " + id, "51 9 9101 1255 - " + new Date().toString());
 	}
 	
 	@GetMapping("person")
